06c01e27d69ff4ad3afd941af96ecc7927b5208a
calvin <calr0x@gmail.com>
2021-07-20 17:40:29 -0500

    .

--- archived/test.sh
+++ archived/test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo $HOSTNAME >> file
+
+echo "source /root/OT-Settings/config.sh"
+source /root/OT-Settings/config.sh
+
+echo "/root/OT-Smoothbrain-Backup/restic snapshots -H $HOSTNAME | grep $HOSTNAME | cut -c1-8 | tail -n 1"
+SNAPSHOT=$(/root/OT-Smoothbrain-Backup/restic snapshots -H $HOSTNAME | grep $HOSTNAME | cut -c1-8 | tail -n 1)
+
+echo $SNAPSHOT >> file
