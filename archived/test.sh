484b1e24eee1403f909499fe5866f62348f84d47
calr0x <52664000+calr0x@users.noreply.github.com>
2021-07-31 07:55:53 -0400

    Delete test.sh

--- archived/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "source /root/OT-Settings/config.sh"
-source /root/OT-Settings/config.sh
-
-echo "/root/OT-Smoothbrain-Backup/restic snapshots -H $HOSTNAME | grep $HOSTNAME | cut -c1-8 | tail -n 1"
-SNAPSHOT=$(/root/OT-Smoothbrain-Backup/restic snapshots -H $HOSTNAME | grep $HOSTNAME | cut -c1-8 | tail -n 1)
-
-echo $SNAPSHOT >> file
